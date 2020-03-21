adbf3dca93b5cea483ebc69d733d460f04042949
Dylan Kilbride <dylankilbride1@gmail.com>
2020-03-21 14:42:11 +0000

    Initial Commit

---
+++ Tech News HubTests/Tech_News_HubTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tech_News_HubTests.swift
+//  Tech News HubTests
+//
+//  Created by Dylan on 21/03/2020.
+//  Copyright © 2020 The Tech News Hub. All rights reserved.
+//
+
+import XCTest
+@testable import Tech_News_Hub
+
+class Tech_News_HubTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
